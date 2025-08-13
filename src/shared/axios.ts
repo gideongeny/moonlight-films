1c59d2317ecdcff48282bdd36979a944e135ad14
Gideon Ngeno <gideongeng.gmail.com>
2025-08-13 13:29:20 +0300

    fix: resolve undefined REACT_APP_API_KEY environment variable

--- src/shared/axios.ts
+++ src/shared/axios.ts
@@ -4,7 +4,7 @@ import { API_URL } from "./constants";
 const instance = axios.create({
   baseURL: API_URL,
   params: {
-    api_key: process.env.REACT_APP_API_KEY,
+    api_key: "8c247ea0b4b56ed2ff7d41c9a833aa77", // TMDB API key
   },
 });
 
